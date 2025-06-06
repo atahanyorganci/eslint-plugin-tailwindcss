0b90a14d0e165d7d9d2b77046346e30f49a9ab22
Atahan Yorgancı <atahanyorganci@hotmail.com>
2025-06-06 15:27:10 +0300

    feat: initial implementation for `classname-order` rule

--- src/rules/classname-order.ts
+++ src/rules/classname-order.ts
@@ -1,5 +1,38 @@
 import type { Rule } from "eslint";
 import type { JSXAttribute } from "estree-jsx";
+import { getTailwindConfig } from "../prettier/config.js";
+
+function arraysEqual<T>(a: T[], b: T[]): boolean {
+	return a.length === b.length && a.every((val, index) => val === b[index]);
+}
+
+const tailwindConfig = await getTailwindConfig({
+	stylesheet: "src/styles/globals.css",
+});
+
+function bigSign(bigIntValue: bigint) {
+	return Number(bigIntValue > 0n) - Number(bigIntValue < 0n);
+}
+
+function reorderClasses(classList: string[]) {
+	const orderedClasses = tailwindConfig.getClassOrder(classList);
+
+	return orderedClasses.sort(([nameA, a], [nameZ, z]) => {
+		// Move `...` to the end of the list
+		if (nameA === "..." || nameA === "…")
+			return 1;
+		if (nameZ === "..." || nameZ === "…")
+			return -1;
+
+		if (a === z)
+			return 0;
+		if (a === null)
+			return -1;
+		if (z === null)
+			return 1;
+		return bigSign(a - z);
+	}).map(([className]) => className);
+}
 
 const classNamesOrder = {
 	meta: {
@@ -33,10 +66,18 @@ const classNamesOrder = {
 					&& node.value.type === "Literal"
 					&& typeof node.value.value === "string"
 				) {
-					context.report({
-						node,
-						message: "TailwindCSS classes should be sorted by category",
-					});
+					const classes = node.value.value.split(" ").filter(Boolean);
+					const orderedClasses = reorderClasses(classes);
+
+					if (!arraysEqual(classes, orderedClasses)) {
+						context.report({
+							node,
+							messageId: "invalidOrder",
+							fix(fixer) {
+								return fixer.replaceText(node.value, `"${orderedClasses.join(" ")}"`);
+							},
+						});
+					}
 				}
 			},
 		};
