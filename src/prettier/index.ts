d55d32fc580bbb45fa461ff6a117b7834cab0715
Atahan Yorgancı <atahanyorganci@hotmail.com>
2025-06-09 15:07:31 +0300

    fix: replace `import.meta.resolve` with `path.resolve`
    Vitest doesn't include `import.meta.resolve` so worker call uses `path.resolve(import.meta.dirname, "worker.js")` to resolve the path to worker script.

--- src/prettier/index.ts
+++ src/prettier/index.ts
@@ -1,11 +1,12 @@
 import type { TailwindConfigParams } from "./config.js";
+import path from "node:path";
 import { createSyncFn } from "synckit";
 
 export interface ReorderParams extends TailwindConfigParams {
 	classes: string[];
 }
 
-const reorderClassesSync = createSyncFn<(options: ReorderParams) => string[]>(import.meta.resolve("./worker.js"));
+const reorderClassesSync = createSyncFn<(options: ReorderParams) => string[]>(path.resolve(import.meta.dirname, "worker.js"));
 
 export function reorderClasses(options: ReorderParams): string[] {
 	return reorderClassesSync(options);
