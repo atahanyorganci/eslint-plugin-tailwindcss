a8202d831da4cd1665e541fc2c9290c54dfffd5c
Atahan Yorgancı <atahanyorganci@hotmail.com>
2025-06-12 13:06:29 +0300

    refactor: remove redundant type cast

--- src/counter.ts
+++ src/counter.ts
@@ -3,7 +3,7 @@ export default class Counter<T> extends Map<T, number> {
 		super();
 		if (iterable) {
 			for (const item of iterable) {
-				const count = this.get(item as T) ?? 0;
+				const count = this.get(item) ?? 0;
 				this.set(item, count + 1);
 			}
 		}
