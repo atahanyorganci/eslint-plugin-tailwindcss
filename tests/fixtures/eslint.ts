7dc428100f884eed84f94f6a8cb88486d3ce9588
Atahan Yorgancı <atahanyorganci@hotmail.com>
2025-06-17 16:59:11 +0300

    refactor: override loaded config with arguments in `loadEslint`

--- tests/fixtures/eslint.ts
+++ tests/fixtures/eslint.ts
@@ -2,9 +2,9 @@ import type { Linter } from "eslint";
 import path from "node:path";
 import { ESLint } from "eslint";
 
-export function loadEslint(baseConfig: Linter.Config = {}): ESLint {
+export function loadEslint(config: Linter.Config = {}): ESLint {
 	return new ESLint({
 		overrideConfigFile: path.resolve(import.meta.dirname, "eslint.config.ts"),
-		baseConfig,
+		overrideConfig: config,
 	});
 }
